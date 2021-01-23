96c09c2a656e656250fe163359c34d587bb2046b
Carlos M. de Lucas <carlanlinux@gmail.com>
2021-01-23 22:31:48 +0100

    fix(CMS-19): Not Found Page with resource route

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useLocation} from 'react-router-dom';
 
-const NotFoundPage = () => (
-<h1>404: Página no encontrada</h1>
-);
+const NotFoundPage = () => {
+    const location = useLocation();
+    console.log(location);
+
+    return(
+        <div>
+            <h1>404: Página no encontrada</h1>
+            <h1>Recurso no encontrado en {location.pathname}</h1>
+        </div>
+    )
+};
 
 export default NotFoundPage;
