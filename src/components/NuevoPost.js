f3fa6b9a047690dbbe775e19c3df1a9961b970dd
Carlos M. de Lucas <carlanlinux@gmail.com>
2021-01-24 12:14:07 +0100

    feat(CMS-38): New post creation

--- src/components/NuevoPost.js
+++ src/components/NuevoPost.js
@@ -1,27 +1,31 @@
 import React, {useState} from 'react';
 import NotFoundPage from "../pages/NotFoundPage";
-import Moment from "react-moment";
+
 
 
 
 const NuevoPost = (props) => {
     const [titulo, setTitulo] = useState('');
     const [texto, setTexto] = useState('');
-    const [nombrePostBD, setnombrePostBD] = useState('');
     const [usuarioExito, setUsuarioExito] = useState('');
 
 
     //Función para añadir un comentario
-    const addUsuario = async () => {
+    const addPost = async (e) => {
+        e.preventDefault();
+        const nombrePost = titulo.replaceAll(" ", "-");
+        const fecha = Date.now();
+        console.log("fecha" + fecha + "Y nombre post" + nombrePost);
         //Lllamamos a la API por post y se le dice que el cuerpo es un JSON donde se pase
         // el nombre de uusuario y el texto del comentario y se le indican las cabeceeras.
-        const result = await fetch(`/api/nuevoUsuario`, {
+        const result = await fetch(`/api/articulos/nuevoArticulo`, {
             method: 'post',
-            body: JSON.stringify({nombre: nombrePostBD, texto: texto, titulo: titulo, fechaCreacion:     <Moment
-                    date={Date.now()}
-                    parse={"YYYY-MM-DD hh:mm"}
-                    format={"MMM-D h:mm a"}
-                />, autor: props.tokenSesion}),
+            body: JSON.stringify({
+                nombre: nombrePost,
+                texto: texto,
+                titulo: titulo,
+                fechaCreacion: fecha,
+                autor: props.tokenSesion}),
             headers: {
                 'Content-Type': 'application/json',
             }
@@ -43,11 +47,11 @@ const NuevoPost = (props) => {
 
     return (
         <>
-        <form id={"add-comment-form"}>
+        <form id={"add-comment-form"} onSubmit={addPost}>
             <h3>Crear un nuevo post</h3>
             <div><span className={"success text-success"}>{usuarioExito}</span></div>
             <label htmlFor={"nombre"}>
-                Nombre:
+                Título:
                 <input id={"nombre"} type={"text"} value={titulo} required onChange={(event => setTitulo(event.target.value))}/>
             </label>
             <label htmlFor={"textoPost"}>
@@ -55,7 +59,8 @@ const NuevoPost = (props) => {
                 <input id={"textoPost"} type={"textarea"} value={texto} required
                           onChange={(event => setTexto(event.target.value))}/>
             </label>
-            <button onClick={() => addUsuario()}>Crear usuario</button>
+            <input type={"submit"} value={"Crear Post"}/>
+
         </form>
 
             </>
