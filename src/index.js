f948806d8c7e18c6bed1e97f259cb50fc2fc8a4c
Carlos M. de Lucas <carlanlinux@gmail.com>
2021-01-25 16:17:05 +0100

    fix(CMS-53): pattern restricting new post publication

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap';
+import'bootstrap/dist/css/bootstrap.css';
 import 'jquery/src/jquery';
 import 'popper.js/dist/popper';
 
